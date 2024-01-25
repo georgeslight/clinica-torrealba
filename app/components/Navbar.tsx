1024a3b49e7bd99593cc3e812902355d605d78f8
George Slight <georgeslight@gmail.com>
2024-01-25 15:54:54 +0100

    change with to fix problem in galaxy phones

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     return (
         <div className='fixed w-full h-20 shadow-xl z-[100]'>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-                <Image src="/assets/Logo_fondo_blanco.png" alt="/" width={300} height={101.25}/>
+                <Image src="/assets/Logo_fondo_blanco.png" alt="/" width={250} height={100}/>
                 <div className='flex-grow'>
                     <ul className='hidden lg:flex justify-center'>
                         <Link href='/'>
