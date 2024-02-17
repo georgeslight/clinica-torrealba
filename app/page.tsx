ca9f4fd3f412338fc359a20e5a944f911693edf0
George Slight <georgeslight@gmail.com>
2024-02-17 15:42:05 +0100

    Added Footer and Contact to page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Navbar from "@/app/components/Navbar";
 import Main from "@/app/components/Main";
 import TeamSlider from "@/app/components/TeamSlider";
 import Expertise from "@/app/components/Expertise";
+import Contact from "@/app/components/Contact";
+import Footer from "@/app/components/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
       <Main />
       <TeamSlider />
       <Expertise />
+      <Contact />
+      <Footer />
     </main>
   );
 }
