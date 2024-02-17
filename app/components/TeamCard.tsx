30df8fd08c4acc1503f67aace1d6f0d98a79cd03
George Slight <georgeslight@gmail.com>
2024-02-17 18:46:49 +0100

    delete shadowe

--- app/components/TeamCard.tsx
+++ app/components/TeamCard.tsx
@@ -13,7 +13,8 @@ const TeamCard = ({
   alt: string;
 }) => {
   return (
-    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group">
+    <div className="relative flex items-center justify-center h-auto w-full rounded-xl group">
+      {/*<div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group">*/}
       <Image className="rounded-md relative" src={src} alt={alt} />
       <div className="hidden group-hover:block absolute bottom-0 left-0 right-0 h-1/3 bg-[#132634] bg-opacity-80 pb-0 transition-all duration-500 ease-in-out transform group-hover:opacity-80 rounded-md">
         <h3 className="text-2xl text-white tracking-wider text-center pt-4">
