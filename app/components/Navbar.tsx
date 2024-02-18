f7d53ad4bfbe29fcae72e806d1b0ce8701bc9a97
George Slight <georgeslight@gmail.com>
2024-02-18 03:22:56 +0100

    side 100% test

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -167,7 +167,7 @@ const Navbar = () => {
         <div
           className={
             side
-              ? "lg:hidden fixed left-0 top-0 w-full h-screen bg-black/70"
+              ? "lg:hidden fixed left-0 top-0 w-full h-[100%] bg-black/70"
               : ""
           }
         >
@@ -175,8 +175,8 @@ const Navbar = () => {
           <div
             className={
               side
-                ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
-                : "fixed left-[-100%] top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 transition-all duration-500"
+                ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-[100%] bg-[#ecf0f3] p-10 ease-in duration-500"
+                : "fixed left-[-100%] top-0 w-[75%] sm:w-[60%] md:w-[45%] h-[100%] bg-[#ecf0f3] p-10 transition-all duration-500"
             }
           >
             <div>
