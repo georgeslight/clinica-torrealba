30b19096dca57bfbe22f8b86106943e741ffc5df
George Slight <georgeslight@gmail.com>
2024-03-10 19:38:18 +0100

    testing isActive conditiion

--- app/ui/FloatingIcons.tsx
+++ app/ui/FloatingIcons.tsx
@@ -20,28 +20,48 @@ const Booking = () => {
             className={`absolute -translate-y-[20%] transition-transform duration-500 group-hover:-translate-y-3/4 ${isActive ? "-translate-y-3/4" : "-translate-y-[20%]"}`}
           >
             <div className="rounded-full bg-[#25D366] p-2.5 w-12 h-12 ml-auto">
-              <Link
-                className={isActive ? "" : "pointer-events-none"}
-                href="https://api.whatsapp.com/send/?phone=56940987902&text&type=phone_number&app_absent=0"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaWhatsapp className="size-7" />
-              </Link>
+              {isActive ? (
+                <Link
+                  href="https://api.whatsapp.com/send/?phone=56940987902&text&type=phone_number&app_absent=0"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaWhatsapp className="size-7" />
+                </Link>
+              ) : (
+                <a
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setIsActive(true);
+                  }}
+                >
+                  <FaWhatsapp className="size-7" />
+                </a>
+              )}
             </div>
           </div>
           <div
             className={`absolute -translate-x-[20%] transition-transform duration-500 group-hover:-translate-x-3/4 ${isActive ? "-translate-x-3/4" : "-translate-x-[20%]"}`}
           >
             <div className="rounded-full bg-[#2cd5c4] p-2.5 w-12 h-12 ml-auto">
-              <Link
-                className={isActive ? "" : "pointer-events-none"}
-                href="https://4c2c5c15e0c38ccb091c5ef98afcb3afac9dce65.agenda.softwaredentalink.com/agenda"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <LuCalendarSearch className="size-7" />
-              </Link>
+              {isActive ? (
+                <Link
+                  href="https://4c2c5c15e0c38ccb091c5ef98afcb3afac9dce65.agenda.softwaredentalink.com/agenda"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <LuCalendarSearch className="size-7" />
+                </Link>
+              ) : (
+                <a
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setIsActive(true);
+                  }}
+                >
+                  <LuCalendarSearch className="size-7" />
+                </a>
+              )}
             </div>
           </div>
         </div>
