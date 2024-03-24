05fd6bd98212216baf0ed4499d2170aa3f0947cc
George Slight <georgeslight@gmail.com>
2024-03-24 18:34:02 +0100

    deleted pading bottom 16

--- app/ui/Main.tsx
+++ app/ui/Main.tsx
@@ -41,7 +41,7 @@ const Main = () => {
   return (
     <div
       id="home"
-      className="w-full h-full mb-16 md:mb-0 pb-16 lg:pt-0 relative bg-black"
+      className="w-full h-full mb-16 md:mb-0 lg:pt-0 relative bg-black"
     >
       <div className="max-w-[1280px] h-full w-full mx-auto">
         <div className="flex flex-col h-screen md:flex-row relative z-10">
