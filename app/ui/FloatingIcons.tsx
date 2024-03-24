523485d998b138180fe3f13cfdc2afb3b4f53438
George Slight <georgeslight@gmail.com>
2024-03-24 14:41:17 +0100

    new floating icons

--- app/ui/FloatingIcons.tsx
+++ app/ui/FloatingIcons.tsx
@@ -1,52 +1,96 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { IoMdAdd } from "react-icons/io";
 import { LuCalendarSearch } from "react-icons/lu";
 import { FaWhatsapp } from "react-icons/fa6";
 
 const Booking = () => {
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(true);
+  const [showCalendar, setShowCalendar] = useState(false);
+
+  const handleClick = () => {
+    if (isActive) {
+      setIsActive(false);
+      setShowCalendar(true);
+    }
+    if (!isActive) {
+      setShowCalendar(false);
+      setIsActive(true);
+    }
+  };
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      // Check if the click event happened inside the component
+      if (!(event.target as HTMLElement).closest(".booking-container")) {
+        // If clicked outside, set isActive to false
+        setShowCalendar(false); // Hide the calendar icon if clicked outside
+        setIsActive(true);
+        // setTimeout(() => {
+        // }, 210);
+      }
+    };
+
+    // Add event listener to detect clicks outside of the component
+    document.body.addEventListener("click", handleOutsideClick);
+
+    // Remove event listener when component unmounts
+    return () => {
+      document.body.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+
   return (
-    <div onClick={() => setIsActive(false)} className="absolute z-50">
+    <>
       <div
-        className={`z-50 fixed bottom-[10%] right-[15%] lg:right-[5%] group ${isActive ? "active" : ""}`}
+        className={`z-50 fixed bottom-6 right-6 ${isActive ? "active" : ""}`}
       >
         <div className="absolute bottom-0 right-0">
           <div
-            className={`absolute -translate-y-[20%] transition-transform duration-500 group-hover:-translate-y-3/4 ${isActive ? "-translate-y-3/4" : "-translate-y-[20%]"}`}
+            className={`transition-all duration-300 transform ease-in-out ${
+              isActive ? "-rotate-90" : ""
+            }`}
+            onClick={handleClick}
           >
-            <div className="rounded-full bg-[#25D366] p-2.5 w-12 h-12 ml-auto">
-              <Link
-                className={
-                  isActive ? "" : "pointer-events-none lg:pointer-events-auto"
-                }
-                href="https://api.whatsapp.com/send/?phone=56940987902&text&type=phone_number&app_absent=0"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaWhatsapp className="size-7" />
-              </Link>
-            </div>
-          </div>
-          <div
-            className={`absolute -translate-x-[20%] transition-transform duration-500 group-hover:-translate-x-3/4 ${isActive ? "-translate-x-3/4" : "-translate-x-[20%]"}`}
-          >
-            <div className="rounded-full bg-[#2cd5c4] p-2.5 w-12 h-12 ml-auto">
-              <Link
-                className={
-                  isActive ? "" : "pointer-events-none lg:pointer-events-auto"
-                }
-                href="https://4c2c5c15e0c38ccb091c5ef98afcb3afac9dce65.agenda.softwaredentalink.com/agenda"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <LuCalendarSearch className="size-7" />
-              </Link>
+            <div
+              className={`rounded-full p-2 w-12 h-12 ml-auto transition-all duration-300 transform ease-in-out ${isActive ? "bg-[#3A3C44]" : "bg-[#2cd5c4]"}`}
+            >
+              {isActive && !showCalendar && (
+                <IoMdAdd className="size-8 text-[#2cd5c4]" />
+              )}
+              {showCalendar && (
+                <Link
+                  href="https://4c2c5c15e0c38ccb091c5ef98afcb3afac9dce65.agenda.softwaredentalink.com/agenda"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <LuCalendarSearch className="size-8" />
+                </Link>
+              )}
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div
+        className={`z-50 fixed bottom-20 right-8 ${isActive ? "active" : ""}`}
+      >
+        <div className="absolute bottom-0 right-0">
+          <div
+            className={`rounded-full p-1.5 w-8 h-8 ml-auto transition-all duration-300 transform ease-in-out ${isActive ? " -rotate-90 scale-0" : "bg-[#25D366]"}`}
+          >
+            {showCalendar && (
+              <Link
+                href="https://api.whatsapp.com/send/?phone=56940987902&text&type=phone_number&app_absent=0"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaWhatsapp className="size-5" />
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ app/ui/FloatingIcons2.tsx
@@ -0,0 +1,53 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { LuCalendarSearch } from "react-icons/lu";
+import { FaWhatsapp } from "react-icons/fa6";
+
+const Booking = () => {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <div onClick={() => setIsActive(false)} className="absolute z-50">
+      <div
+        className={`z-50 fixed bottom-[10%] right-[15%] lg:right-[5%] group ${isActive ? "active" : ""}`}
+      >
+        <div className="absolute bottom-0 right-0">
+          <div
+            className={`absolute -translate-y-[20%] transition-transform duration-500 group-hover:-translate-y-3/4 ${isActive ? "-translate-y-3/4" : "-translate-y-[20%]"}`}
+          >
+            <div className="rounded-full bg-[#25D366] p-2.5 w-12 h-12 ml-auto">
+              <Link
+                className={
+                  isActive ? "" : "pointer-events-none lg:pointer-events-auto"
+                }
+                href="https://api.whatsapp.com/send/?phone=56940987902&text&type=phone_number&app_absent=0"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaWhatsapp className="size-7" />
+              </Link>
+            </div>
+          </div>
+          <div
+            className={`absolute -translate-x-[20%] transition-transform duration-500 group-hover:-translate-x-3/4 ${isActive ? "-translate-x-3/4" : "-translate-x-[20%]"}`}
+          >
+            <div className="rounded-full bg-[#2cd5c4] p-2.5 w-12 h-12 ml-auto">
+              <Link
+                className={
+                  isActive ? "" : "pointer-events-none lg:pointer-events-auto"
+                }
+                href="https://4c2c5c15e0c38ccb091c5ef98afcb3afac9dce65.agenda.softwaredentalink.com/agenda"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <LuCalendarSearch className="size-7" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Booking;
