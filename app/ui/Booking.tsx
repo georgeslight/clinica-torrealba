38d450f6a1df03e5580fa8da9b0f71ab86f0cd22
George Slight <georgeslight@gmail.com>
2024-03-25 17:08:24 +0100

    fixed boking component for mobile version

--- app/ui/Booking.tsx
+++ app/ui/Booking.tsx
@@ -6,21 +6,11 @@ import React from "react";
 const Booking = () => {
   return (
     <div id="booking" className="relative w-full h-full py-16">
-      <div className="mx-auto w-full h-full flex-col justify-center grid grid-cols-3">
-        {/* Left Side - Image */}
-        <div className="relative col-span-2">
-          <Image
-            src={monito}
-            alt="monito"
-            quality={100}
-            className="w-full max-h-96 object-fill"
-          />
-          <div className="absolute max-h-96 w-1/2 inset-0 bg-gradient-to-r from-transparent to-[#ecf0f3] left-1/2" />
-        </div>
-        {/* Right Side */}
-        <div className="relative z-10 col-span-1 flex justify-center items-center bg-[#ecf0f3]">
-          <div className="max-w-[1240px] mx-auto w-full h-full flex flex-col justify-center items-center">
-            <h2 className="font-didot tracking-widest uppercase text-[#2cd5c4] pb-10">
+      <div className="mx-auto w-full h-full flex-col justify-center grid grid-cols-1 md:grid-cols-3 gap-4">
+        {/* Left Side - Text */}
+        <div className=" absolute z-10 md:hidden flex items-center justify-center inset-0">
+          <div className="relative mx-auto px-4 text-center">
+            <h2 className="font-didot tracking-widest uppercase pb-4 text-[#2cd5c4]">
               Reserva tu cita
             </h2>
             <Link
@@ -28,6 +18,32 @@ const Booking = () => {
               target="_blank"
               rel="noopener noreferrer"
               className="px-8 py-2 font-bold text-white bg-[#2cd5c4] rounded-2xl hover:bg-[#1bb3a6] hover:scale-105 ease-in-out duration-300"
+            >
+              Agenda por Dentalink
+            </Link>
+          </div>
+        </div>
+        {/* Image */}
+        <div className="relative col-span-3 md:col-span-2">
+          <Image
+            src={monito}
+            alt="monito"
+            quality={100}
+            className="w-full h-full object-fill md:max-h-96"
+          />
+          <div className="absolute inset-0 bg-gradient-to-r from-transparent to-[#ecf0f3]  w-1/2 left-1/2" />
+        </div>
+        {/* Right Side */}
+        <div className="hidden md:relative z-10 md:flex justify-center items-center bg-[#ecf0f3]">
+          <div className="max-w-[320px] mx-auto px-4">
+            <h2 className="font-didot tracking-widest uppercase text-[#2cd5c4] text-left pb-10">
+              Reserva tu cita
+            </h2>
+            <Link
+              href="https://4c2c5c15e0c38ccb091c5ef98afcb3afac9dce65.agenda.softwaredentalink.com/agenda"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-full px-8 sm:px-4 lg:px-8 py-2 font-bold text-white bg-[#2cd5c4] rounded-2xl hover:bg-[#1bb3a6] hover:scale-105 ease-in-out duration-300"
             >
               Agenda por Dentalink
             </Link>
