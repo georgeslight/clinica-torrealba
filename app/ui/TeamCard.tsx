8a84cf1a6302cf5a4be4a19b705505ca099c705d
George Slight <georgeslight@gmail.com>
2024-10-07 20:02:42 +0200

    team card, still have to change size in mobile

--- app/ui/TeamCard.tsx
+++ app/ui/TeamCard.tsx
@@ -9,18 +9,22 @@ const TeamCard = ({
 }: {
   src: StaticImageData;
   title: string;
-  description: string;
+  description: string[];
   alt: string;
 }) => {
   return (
     <div className="relative flex items-center justify-center h-auto w-full rounded-xl group">
       {/*<div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group">*/}
       <Image className="rounded-md relative" src={src} alt={alt} />
-      <div className="hidden group-hover:block absolute bottom-0 left-0 right-0 h-1/3 bg-[#132634] bg-opacity-80 pb-0 transition-all duration-500 ease-in-out transform group-hover:opacity-80 rounded-md">
-        <h3 className="text-2xl text-white tracking-wider text-center pt-4">
+      <div className=" absolute bottom-0 left-0 right-0 h-1/3 bg-[#132634] bg-opacity-80 pb-0 transition-all duration-500 ease-in-out transform opacity-75 group-hover:opacity-100 rounded-b-md">
+        <h3 className="text-xl text-white tracking-wider text-center pt-4">
           {title}
         </h3>
-        <p className="pt-2 text-white text-center">{description}</p>
+        <ul className="pt-2 text-white text-left space-y-auto list-disc list-inside m-auto w-fit">
+          {description.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
