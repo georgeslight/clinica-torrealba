b4080c1e5dd1660b53b0324d1de96d9430893b0d
George Slight <georgeslight@gmail.com>
2024-10-13 09:37:10 +0200

    fotos estetica

--- lib/aesthetics.tsx
+++ lib/aesthetics.tsx
@@ -1,29 +1,44 @@
-import test from "@/public/medicina-estetica/test.png";
+import first from "@/public/medicina-estetica/estetica1.png";
+import second from "@/public/medicina-estetica/estetica2.png";
+import third from "@/public/medicina-estetica/estetica3.png";
+import fourth from "@/public/medicina-estetica/estetica4.png";
+import fifth from "@/public/medicina-estetica/estetica5.png";
+import sixth from "@/public/medicina-estetica/estetica6.png";
 import { StaticImageData } from "next/image";
 
 export const aesthetics: {
   name: string;
-  description: string;
+  // description: string;
   image: { src: StaticImageData; alt: string };
 }[] = [
   {
-    name: "Ortodoncia convencional metálica",
-    description: "Realizado con brackets de acero inoxidable.",
-    image: { src: test, alt: "image alt" },
+    name: "",
+    // description: "Realizado con brackets de acero inoxidable.",
+    image: { src: first, alt: "image alt" },
   },
   {
-    name: "Ortodoncia convencional metálica",
-    description: "Realizado con brackets de acero inoxidable.",
-    image: { src: test, alt: "image alt" },
+    name: "",
+    // description: "Realizado con brackets de acero inoxidable.",
+    image: { src: second, alt: "image alt" },
   },
   {
-    name: "Ortodoncia convencional metálica",
-    description: "Realizado con brackets de acero inoxidable.",
-    image: { src: test, alt: "image alt" },
+    name: "",
+    // description: "Realizado con/brackets de acero inoxidable.",
+    image: { src: third, alt: "image alt" },
   },
   {
-    name: "Ortodoncia convencional metálica",
-    description: "Realizado con brackets de acero inoxidable.",
-    image: { src: test, alt: "image alt" },
+    name: "",
+    // description: "Realizado con/brackets de acero inoxidable.",
+    image: { src: fourth, alt: "image alt" },
+  },
+  {
+    name: "",
+    // description: "Realizado con brackets de acero inoxidable.",
+    image: { src: fifth, alt: "image alt" },
+  },
+  {
+    name: "",
+    // description: "Realizado con brackets de acero inoxidable.",
+    image: { src: sixth, alt: "image alt" },
   },
 ];
