9645fd9ca7a5a328e8a4b14f9cf5e4bf5efe56d7
georgeslight <liggeros@proton.me>
2025-12-06 18:08:05 +0100

    flake build testing with bun2nix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,11 @@ import Navbar from "@/app/ui/Navbar";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import local from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 const didot = local({
   src: [
---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  images: {
+    // Add all qualities used in your components
+    qualities: [50, 75, 100],
+  },
+};
